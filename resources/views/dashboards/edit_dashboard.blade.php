677f41eed603cc9a535be107b391535414e94359
Rafał <bragiello93@gmail.com>
2017-07-11 14:34:42 +0200

    new main styles for dashboard

--- resources/views/dashboards/edit_dashboard.blade.php
+++ resources/views/dashboards/edit_dashboard.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 @section('content')
 <div class="row">
@@ -5,21 +6,21 @@
         <form class="form-horizontal" method="post" action="/home/change/{{$dash->id}}">
             <legend>You're editing dash nr {{$dash->id}}</legend>
                 {{csrf_field()}}
-                <div class="form-group">
-                    <label for="title_edit">Title: </label>
-                    <input class="form-control" type="text" value="{{$dash->title}}"
+                <div class="form-group title_layout">
+                    <label for="title_edit" class="label_content">Title: </label>
+                    <input class="form-control edit_input" type="text" value="{{$dash->title}}"
                            name="title" id="title_edit">
                 </div>
                 <div class="form-group">
-                    <label for="body_edit">Body Panel:</label>
-                    <textarea rows="7" class="form-control" id="body_edit" name="body">
+                    <label for="body_edit" class="label_content">Body Panel:</label>
+                    <textarea rows="7" class="form-control edit_text" name="body">
                         {{$dash->body}}
                     </textarea>
                 </div>
                 <input type="submit" value="change" class="btn">
         </form>
 
-        <a href="{{url('home')}}"><button class="btn btn-danger"> Cancel</button></a>
+        <a href="{{url('home')}}"><button class="btn btn-danger">Back</button></a>
     </div>
 </div>
 @endsection
