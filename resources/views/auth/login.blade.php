bb407203c493833ee2a8bdbb33bd2c9487d65c4e
Rafał <bragiello93@gmail.com>
2017-07-12 09:32:34 +0200

    new login/register button styles /new discover panel/ new after login layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-2" id="login_form_layout">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
@@ -50,7 +50,7 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="custom_button_login">
                                     Login
                                 </button>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,15 +44,15 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a id="right_nav_guest_login" href="{{ route('login') }}">Login</a></li>
+                            <li><a id="right_nav_guest_register" href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
                                 <a id="right_nav_drop_logout" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
+                                <ul class="dropdown-menu" role="menu" >
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
