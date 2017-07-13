4e8e36a6b4765c5ae862024ae0214dca570a60b5
Rafał <bragiello93@gmail.com>
2017-07-13 13:36:02 +0200

    improved responsive design

--- resources/views/dashboards/all_dashboards_subject.blade.php
+++ resources/views/dashboards/all_dashboards_subject.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 @section('content')
+
 @foreach($subjects as $sub)
 
     <div class="dashboard_subject_section">
     <p> Topic: {{key($sub)}}</p>
     <p>Quantity: {{$sub[key($sub)]}}</p>
         <a href="/dashboards/{{key($sub)}}">show</a>
-        <hr>
+
     </div>
 
     @endforeach
