4efa50b8d85a16de56416cdd44be1b2187c39056
Rafał <bragiello93@gmail.com>
2017-07-13 15:14:17 +0200

    responsive design

--- resources/views/auth/discover.blade.php
+++ resources/views/auth/discover.blade.php
@@ -4,7 +4,7 @@
     @foreach ($discover as $eachDisc)
 <div class="container">
   <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 discover_random_dash">
+    <div class="col-md-8 discover_random_dash">
       <div class="discover_random_title"><p>Title: {{$eachDisc->title}}</p></div>
       <div class="discover_random_body"><p>Body: {{$eachDisc->body}}</p></div>
     </div>
